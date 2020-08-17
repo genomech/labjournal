c36b552cffeb1f7608fa439054f1224dc9ad62ca
Emil Valeev <regnveig@yandex.ru>
2020-08-17 16:40:50 +0700

    -

--- scripts/find_snips.py
+++
@@ -1,5 +0,0 @@
-from lib.blister import *
-
-coverage = pd.read_csv("/dev/datasets/FairWind/_results/Andre/cuffdiff/genes.fpkm_tracking", sep='\t')
-coverage.drop(columns=['tracking_id', 'class_code', 'nearest_ref_id'], inplace=True, axis=1)
-print(coverage)
